571944120302cc3fe3c9c548c036febe04ea5871
gpego1 <gpego786@gmail.com>
2025-01-15 13:03:33 -0300

    Projeto criado

---
+++ src/main/java/org/gabrielpego/mongodbb/MongodbbApplication.java
@@ -0,0 +1,13 @@
+package org.gabrielpego.mongodbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gabrielpego/mongodbb/MongodbbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gabrielpego.mongodbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
