d4abdfd134fffb8da4c616cff739ed9024113df2
gpego1 <gpego786@gmail.com>
2025-01-15 20:53:15 -0300

    Buscando usuario por Id + tratamento de execoes

--- src/main/java/org/gabrielpego/mongodbb/services/UserService.java
+++ src/main/java/org/gabrielpego/mongodbb/services/UserService.java
@@ -2,10 +2,12 @@ package org.gabrielpego.mongodbb.services;
 
 import org.gabrielpego.mongodbb.domain.User;
 import org.gabrielpego.mongodbb.repository.UserRepository;
+import org.gabrielpego.mongodbb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,4 +19,8 @@ public class UserService {
         return repo.findAll();
     }
 
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
 }
---
+++ src/main/java/org/gabrielpego/mongodbb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package org.gabrielpego.mongodbb.services.exception;
+
+import java.io.Serial;
+
+public class ObjectNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
