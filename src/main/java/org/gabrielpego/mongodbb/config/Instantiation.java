028d0b6057b5f89cd90e191921b187455e8188e1
gpego1 <gpego786@gmail.com>
2025-01-15 22:54:01 -0300

    Acrescentando comentarios aos posts

--- src/main/java/org/gabrielpego/mongodbb/config/Instantiation.java
+++ src/main/java/org/gabrielpego/mongodbb/config/Instantiation.java
@@ -3,6 +3,7 @@ package org.gabrielpego.mongodbb.config;
 import org.gabrielpego.mongodbb.domain.Post;
 import org.gabrielpego.mongodbb.domain.User;
 import org.gabrielpego.mongodbb.dto.AuthorDTO;
+import org.gabrielpego.mongodbb.dto.CommentDTO;
 import org.gabrielpego.mongodbb.repository.PostRepository;
 import org.gabrielpego.mongodbb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,12 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", new AuthorDTO(maria));
         Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
