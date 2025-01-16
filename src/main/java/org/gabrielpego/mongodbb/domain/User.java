3f3a9bad7701ab4d905cf56a1fdf529c734c7eaa
gpego1 <gpego786@gmail.com>
2025-01-15 22:27:07 -0300

    referenciando os posts dom usuario

--- src/main/java/org/gabrielpego/mongodbb/domain/User.java
+++ src/main/java/org/gabrielpego/mongodbb/domain/User.java
@@ -1,10 +1,13 @@
 package org.gabrielpego.mongodbb.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -17,6 +20,9 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef
+    private List<Post> posts = new ArrayList<>();
+
     public User(){}
 
     public User(String id, String name, String email) {
@@ -26,15 +32,16 @@ public class User implements Serializable {
     }
 
     public String getId() {return id;}
-
     public void setId(String id) {this.id = id;}
 
     public String getName() {return name;}
     public void setName(String name) {this.name = name;}
 
     public String getEmail() {return email;}
-
     public void setEmail(String email) {this.email = email;}
+
+    public List<Post> getPosts() { return posts; }
+    public void setPosts(List<Post> posts) { this.posts = posts; }
 
     @Override
     public boolean equals(Object o) {
