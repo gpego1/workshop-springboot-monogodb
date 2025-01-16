5024eb415f0a0d46b7e514b32d080a742b0df305
gpego1 <gpego786@gmail.com>
2025-01-15 22:42:31 -0300

    Obtendo posts por ID

---
+++ src/main/java/org/gabrielpego/mongodbb/services/PostService.java
@@ -0,0 +1,21 @@
+package org.gabrielpego.mongodbb.services;
+
+import org.gabrielpego.mongodbb.domain.Post;
+import org.gabrielpego.mongodbb.repository.PostRepository;
+import org.gabrielpego.mongodbb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
