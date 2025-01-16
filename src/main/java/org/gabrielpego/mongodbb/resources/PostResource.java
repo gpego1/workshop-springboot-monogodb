6c5c5ee81cdf221ff2c2f71314f3284df8f2b159
gpego1 <gpego786@gmail.com>
2025-01-15 23:16:32 -0300

    Consultas simples com query methods

--- src/main/java/org/gabrielpego/mongodbb/resources/PostResource.java
+++ src/main/java/org/gabrielpego/mongodbb/resources/PostResource.java
@@ -1,10 +1,13 @@
 package org.gabrielpego.mongodbb.resources;
 
 import org.gabrielpego.mongodbb.domain.Post;
+import org.gabrielpego.mongodbb.resources.util.URL;
 import org.gabrielpego.mongodbb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,6 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @RequestMapping(value="/titlesearch", method= RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
